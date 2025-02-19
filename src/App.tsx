0a885aea6f1d730db7e63075eb0705f2a5252800
Võ Hữu Tài <90029952+vohuutai23@users.noreply.github.com>
2025-02-19 22:27:06 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="bg-gray-100 h-screen flex items-center justify-center">
+      <h1 className="text-xl text-red-900 font-bold">
+        Hello Vite + React + Tailwind CSS
+      </h1>
+    </div>
+  );
+};
+
+export default App;
